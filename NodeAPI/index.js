4bd0ac7bedaf0aebf8e205d82c2db1cc3748f983
vnak-3 <mrznak88k@gmail.com>
2026-03-09 00:18:24 +0700

    .

--- NodeAPI/index.js
+++ NodeAPI/index.js
@@ -1,17 +1,24 @@
-// IMPORT Express Server
 const e1 = require('express');
 const app = e1();
 
-//SEARCH API
+// GET APIs
 app.get('/search', (req, res) => {
     res.send('<html><body>INSIDE SEARCH API..</body></html>');
 });
-
-//VIEW API
 app.get('/view', (req, res) => {
     res.send('<html><body>INSIDE VIEW API..</body></html>');
 });
 
-// START THE EXPRESS SERVER
-app.listen(3000, () =>
-    console.log('EXPRESS Server Started at Port No: 3000'));
+// NEW APIs
+app.post('/login', (req, res) => {
+    res.send('<html><body>INSIDE LOGIN API..</body></html>');
+});
+app.put('/updateprofile', (req, res) => {
+    res.send('<html><body>INSIDE UPDATE PROFILE API..</body></html>');
+});
+app.delete('/del', (req, res) => {
+    res.send('<html><body>INSIDE DELETE API..</body></html>');
+});
+
+app.listen(5000, () =>
+    console.log('EXPRESS Server Started at Port No: 5000'));
